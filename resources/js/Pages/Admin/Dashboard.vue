f5bc2c4463f4e7d5656e3936fdc6f9ebde345da2
akwancakra <akwancakra20@gmail.com>
2024-10-12 20:27:35 +0700

    feat: add dashboard layout

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3'
+import DashboardLayout from '@/Layouts/DashboardLayout.vue'
+
+</script>
+
+<template>
+
+    <Head>
+        <title>Dashboard Admin</title>
+    </Head>
+
+    <DashboardLayout>
+        <template #header>
+            <h1 class="text-xl font-semibold tracking-tight">Dashboard Admin</h1>
+        </template>
+
+        <section class="bg-white p-3 rounded-lg mb-3">
+            <div class="mb-3">
+                <p class="font-semibold text-2xl tracking-tight">Dashboard Admin</p>
+                <p class="text-sm text-neutral-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
+                    veniam,
+                    vero omnis dolores temporibus sed?</p>
+            </div>
+        </section>
+    </DashboardLayout>
+</template>

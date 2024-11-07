1dfeabbf30a00f9529d6ffe8c81e35d652cca9a8
Billdan Satriana <billdansr07@gmail.com>
2024-11-07 17:31:36 +0700

    + Adding $guarded to all models

--- app/Models/Ambassador.php
+++ app/Models/Ambassador.php
@@ -10,6 +10,10 @@ class Ambassador extends Model
     /** @use HasFactory<\Database\Factories\AmbassadorFactory> */
     use HasFactory;
 
+    protected $guarded = [];
+
+    protected $hidden = ['phone_number'];
+
     public function revenues() {
         return $this->hasMany(Income::class);
     }

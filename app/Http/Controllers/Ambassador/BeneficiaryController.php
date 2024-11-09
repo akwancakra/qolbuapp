dce87453f96c455f43c1f20f4363fa026a54d21f
Billdan Satriana <billdansr07@gmail.com>
2024-11-09 17:26:48 +0700

    Searching, Filtering, Sorting, Paginate

--- app/Http/Controllers/Ambassador/BeneficiaryController.php
+++ app/Http/Controllers/Ambassador/BeneficiaryController.php
@@ -12,9 +12,15 @@ class BeneficiaryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $beneficiaries = Beneficiary::all();
+        $beneficiaries = Beneficiary::query()
+            ->search($request->search)
+            ->age($request->min_age, $request->max_age)
+            ->educationLevel($request->education_level)
+            ->status($request->status)
+            ->sortBy($request->input('sort_by', 'created_at'), $request->input('sort_direction', 'desc'))
+            ->paginate(10);
 
         return Inertia::render('Ambassador/Beneficiaries/Index', compact('beneficiaries'));
     }
--- app/Http/Controllers/BoardMember/BeneficiaryController.php
+++ app/Http/Controllers/BoardMember/BeneficiaryController.php
@@ -13,9 +13,15 @@ class BeneficiaryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $beneficiaries = Beneficiary::all();
+        $beneficiaries = Beneficiary::query()
+            ->search($request->search)
+            ->age($request->min_age, $request->max_age)
+            ->educationLevel($request->education_level)
+            ->status($request->status)
+            ->sortBy($request->input('sort_by', 'created_at'), $request->input('sort_direction', 'desc'))
+            ->paginate(10);
         
         return Inertia::render('BoardMember/Beneficiaries/Index', compact('beneficiaries'));
     }

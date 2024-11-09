2cc8b984fb2270a0975b38a68a1eeb20b2373e50
Billdan Satriana <billdansr07@gmail.com>
2024-11-09 19:15:12 +0700

    Added validation for query parameters

--- app/Http/Controllers/Ambassador/BeneficiaryController.php
+++ app/Http/Controllers/Ambassador/BeneficiaryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Ambassador;
 use App\Http\Controllers\Controller;
 use App\Models\Beneficiary;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Inertia\Inertia;
 
 class BeneficiaryController extends Controller
@@ -14,6 +15,17 @@ class BeneficiaryController extends Controller
      */
     public function index(Request $request)
     {
+        $request->validate([
+            'search' => 'nullable|string',
+            'min_age' => 'nullable|integer|min:0',
+            'max_age' => 'nullable|integer|gt:min_age',
+            'education_level' => 'nullable|string',
+            'school_grade' => 'nullable|integer|gt:0',
+            'status' => ['nullable', Rule::in(['Yatim', 'Piatu', 'Yatim Piatu', 'Dhuafa'])],
+            'sort_by' => 'nullable|string',
+            'sort_direction' => 'nullable|string|in:asc,desc',
+        ]);
+
         $beneficiaries = Beneficiary::query()
             ->search($request->search)
             ->age($request->min_age, $request->max_age)
